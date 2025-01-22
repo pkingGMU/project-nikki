15dfd69582083b583a0717f8bf1d1edcda91f8fd
pking23 <pking23@gmu.edu>
2025-01-22 13:12:42 -0500

    read midi csv features

--- main.lua
+++ main.lua
@@ -1,9 +1,43 @@
+---@diagnostic disable: need-check-nil
 -- Global Imports --
 package.path = package.path .. ";/usr/share/lua/5.4/?.lua"
-local Class = require("hump.class")
-local Gamestate = require("hump.gamestate")
+local Class = require("libraries.hump-master.class")
+local Gamestate = require("libraries.hump-master.gamestate")
+local csv = require("libraries.lua-csv-master.lua.csv")
+
+-- On load open csv file --
+local midi_file_csv = csv.open("sounds/midi_training.csv")
+-- Fetch the first row (header)
+local header = midi_file_csv:lines()()
+
+-- Keep track of line count --
+midi_line_count = 1
 
 
+if header then
+    -- Print the header to check it
+    print("CSV Header: ", table.concat(header, ", "))
+else
+    print("No header found in CSV.")
+end
+
+for fields in midi_file_csv:lines() do
+
+    if midi_line_count < 2 or midi_line_count > 4 then
+        goto continue
+    end
+    
+    for i, v in ipairs(fields) do 
+        print(i,v)
+    end
+    
+    ::continue::
+    midi_line_count = midi_line_count + 1
+    
+    
+end
+
+print("Hello World")
 
 -- Local imports --
 require("states.baseWindow")
@@ -24,6 +58,7 @@ function love.load()
 
     
 
+
     
 end
 
@@ -33,6 +68,8 @@ end
 function menu:init()
     local window = baseWindow()
     window:init()
+
+    
 
     -- Load test image
     pfp_test = love.graphics.newImage("assets/img/pfp.jpg")
@@ -46,7 +83,8 @@ end
 function menu:draw()
     if b_down == false then
         love.graphics.setColor(1, 1, 1)
-        love.graphics.draw(pfp_test, 50, 50)
+        -- love.graphics.draw(pfp_test, 50, 50) --
+        love.graphics.rectangle("fill", 50, 50, 100, 100)
     end
     love.graphics.setColor(255, 0, 0)
     love.graphics.print(Num, 0, 0)
