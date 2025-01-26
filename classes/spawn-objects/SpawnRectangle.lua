926718784969fa63b0a2f8b47c4965e8eaf535e5
pking23 <pking23@gmu.edu>
2025-01-26 17:18:16 -0500

    added options to add color to rectangle

--- classes/spawn-objects/SpawnRectangle.lua
+++ classes/spawn-objects/SpawnRectangle.lua
@@ -5,10 +5,13 @@ local Class = require("libraries.hump-master.class")
 
 SpawnRectangle = Class()
 
-function SpawnRectangle:init(pos_x, pos_y)
+function SpawnRectangle:init(pos_x, pos_y, r, g, b)
     self.x = pos_x
     self.y = pos_y
     self.width = 100
     self.height = 100
+    self.r = r
+    self.g = g
+    self. b = b
 end
 
