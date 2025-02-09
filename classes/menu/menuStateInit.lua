b1b23126e1e1acec1c78d4fff144f17abc1d9be1
pkingGMU <pking23@gmu.edu>
2025-02-09 16:17:46 +0000

    Plays main menu music

--- classes/menu/menuStateInit.lua
+++ classes/menu/menuStateInit.lua
@@ -36,6 +36,11 @@ function menuStateInit:init()
     -- First button --
     self.play_button = button(self.button_pos_x, self.button_pos_y, self.button_width, self.button_height,1,1,1)
 
+    -- Menu Music --
+    self.menu_song = love.audio.newSource("sounds/Main_Menu/8bit.mp3", 'stream')
+    self.menu_song:setLooping(true)
+    self.menu_song:play()
+
     
 end
 
