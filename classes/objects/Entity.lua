b81ac1b8779ef672892fa269a4722272d00fffda
pking23 <pking23@gmu.edu>
2025-03-08 18:00:34 -0500

    moved parameters from object class to entity class

--- classes/objects/Entity.lua
+++ classes/objects/Entity.lua
@@ -6,7 +6,7 @@ require('classes.objects.Object')
 -- Parent class Object --
 Entity = Class{__includes = Object}
 
-function Entity:init(xvel, yvel, speed)
+function Entity:init(xvel, yvel, speed, friction, jump_vel, can_jump)
     Object.init(self)
 
     self.xvel = xvel or 0
