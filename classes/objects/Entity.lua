d756bc3d39c192afc5148fa13fd3c61d15e8b096
pking23 <pking23@gmu.edu>
2025-03-08 18:00:56 -0500

    moved parameters from object class to entity class

--- classes/objects/Entity.lua
+++ classes/objects/Entity.lua
@@ -12,6 +12,11 @@ function Entity:init(xvel, yvel, speed, friction, jump_vel, can_jump)
     self.xvel = xvel or 0
     self.yvel = yvel or 0
     
+    self.friction = friction or 5
+    self.speed = speed or 1500
+    self.jump_vel = jump_vel or -800
+    self.can_jump = can_jump or true
+    
 
 end
 
--- classes/objects/Object.lua
+++ classes/objects/Object.lua
@@ -5,26 +5,19 @@ local Class = require("libraries.hump-master.class")
 
 Object = Class()
 
-function Object:init(x, y, z, rot, scaleX, scaleY, w, h, friction, speed, jump_vel, can_jump)
+function Object:init(x, y, z, rot, w, h, scaleX, scaleY)
     self.x = x or 0
     self.y = y or 0
     self.z = z or 0
+    self.w = w or 50
+    self.h = h or 50
     self.rot = rot or 0
     self.scaleX = scaleX or 1
     self.scaleY = scaleY or 1
-    self.w = w or 50
-    self.h = h or 50
-    self.friction = friction or 5
-    self.speed = speed or 1500
-    self.jump_vel = jump_vel or -800
-    self.can_jump = can_jump or true
 end
 
 function Object:draw()
     love.graphics.push()
-    --love.graphics.translate(self.x, self.y)
-    --love.graphics.rotate(self.rot)
-    --love.graphics.scale(self.scaleX, self.scaleY)
     
     -- Draw the rectangle (or image for your entity)
     love.graphics.rectangle("fill", self.x, self.y, self.w, self.h)
