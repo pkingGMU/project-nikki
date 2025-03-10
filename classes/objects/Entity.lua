da4b650322f32fe2373f8700b4c5a67c76af7dfc
pking23 <pking23@gmu.edu>
2025-03-10 08:19:01 -0400

    added update function to entity class

--- classes/objects/Entity.lua
+++ classes/objects/Entity.lua
@@ -6,18 +6,27 @@ require('classes.objects.Object')
 -- Parent class Object --
 Entity = Class{__includes = Object}
 
-function Entity:init(xvel, yvel, speed, friction, jump_vel, can_jump)
-    Object.init(self)
+function Entity:init(params)
+    Object.init(self, params)
 
-    self.xvel = xvel or 0
-    self.yvel = yvel or 0
+    self.xvel = params.xvel or 0
+    self.yvel = params.yvel or 0
     
-    self.friction = friction or 5
-    self.speed = speed or 1500
-    self.jump_vel = jump_vel or -800
-    self.can_jump = can_jump or true
-    
+    self.friction = params.friction or 5
+    self.speed = params.speed or 1500
+    self.jump_vel = params.jump_vel or -800
+    self.can_jump = params.can_jump or true
 
+    self.health = params.health or 100
+
+end
+
+function Entity:update()
+    Object.update(self)
+end
+
+function Entity:takeDamage(damage)
+    self.health = self.health - damage
 end
 
 
