82b6c1d804e727136b104eef89466164242fae11
pking23 <pking23@gmu.edu>
2025-03-18 14:12:40 -0400

    Added object Handler for future use

---
+++ classes/objects/ObjectHandler.lua
@@ -0,0 +1,24 @@
+-- Local Imports --
+local Class = require("libraries.hump-master.class")
+
+
+ObjectHandler = Class()
+
+local string_key
+
+function ObjectHandler:init()
+    self.object_table = {}
+    self.object_idx = 0
+end
+
+function ObjectHandler:addObject(object --[[]])
+
+    
+
+end
+
+function ObjectHandler:update(dt)
+
+    
+end
+
