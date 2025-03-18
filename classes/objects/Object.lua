7f7df7c38131e1053123ed264d918aedcfd5f620
pking23 <pking23@gmu.edu>
2025-03-18 14:12:29 -0400

    Auto add object to obejct handler

--- classes/objects/Object.lua
+++ classes/objects/Object.lua
@@ -2,10 +2,12 @@ local Class = require("libraries.hump-master.class")
 
 -- Local Imports --
 
+local string_key
+
 
 Object = Class()
 
-function Object:init(params)
+function Object:init(params, objectHandler)
     self.x = params.x or 0
     self.y = params.y or 0
     self.z = params.z or 0
@@ -14,9 +16,17 @@ function Object:init(params)
     self.rot = params.rot or 0
     self.scaleX = params.scaleX or 1
     self.scaleY = params.scaleY or 1
+    self.type = {1}
+   
 
     self.centerX = self.x + self.w / 2
     self.centerY = self.y + self.h / 2
+
+    self.can_collide = params.can_collide or false
+
+    
+    Object:addToHandler(self, objectHandler)
+
 end
 
 function Object:draw()
@@ -39,3 +49,26 @@ end
 function Object:testPrint(word --[[string]])
     print(word)
 end
+
+function Object:addToHandler(object --[[table]], objectHandler --[[table]])
+
+    objectHandler.object_idx = objectHandler.object_idx + 1
+    string_key = 'entity' .. tostring(objectHandler.object_idx)
+    table.insert(objectHandler.object_table, object)
+
+    print("Inserted " .. string_key)
+    print(object.can_collide)
+
+end
+
+function Object:checkCollisions(objectHandler)
+
+        
+        
+        
+
+end
+
+    
+
+
