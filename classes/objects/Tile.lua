8ca6d702693d58f5d818a14459f0fb015a5fcfe8
pking23 <pking23@gmu.edu>
2025-03-19 10:42:47 -0400

    Add to tile handler function and draw function

--- classes/objects/Tile.lua
+++ classes/objects/Tile.lua
@@ -1,15 +1,34 @@
+local Class = require("libraries.hump-master.class")
+
+
 -- Local Imports --
 require('classes.objects.Object')
 
 -- Parent class Object --
 Tile = Class{__includes = Object}
 
-function Tile:init(params)
-    Object.init(self, params)
+function Tile:init(params, objectHandler, tileHandler)
+    Object.init(self, params, objectHandler)
+
+    Tile:addToTileHandler(self, tileHandler)
 end
 
 function Tile:update()
     Object.update(self)
+end
+
+function Tile:addToTileHandler(tile, tileHandler)
+
+    tileHandler.tile_idx = tileHandler.tile_idx + 1
+    local string_key = 'tile'.. tostring(tileHandler.tile_idx)
+    table.insert(tileHandler.tile_table, tile)
+    
+
+end
+
+
+function Tile:draw()
+love.graphics.rectangle("line", self.x , self.y , self.w, self.h)
 end
 
 
