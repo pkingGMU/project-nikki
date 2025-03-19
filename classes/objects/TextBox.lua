5dd30329bb0440c5ebfe452ac1ae37aedbebdad1
pking23 <pking23@gmu.edu>
2025-03-19 10:41:00 -0400

    New text box object

---
+++ classes/objects/TextBox.lua
@@ -0,0 +1,36 @@
+local Class = require("libraries.hump-master.class")
+
+
+-- Local Imports --
+require('classes.objects.Object')
+
+
+-- Parent class Object --
+TextBox = Class{__includes = Object}
+
+function TextBox:init(params, objectHandler, npc)
+    Object.init(self, params, objectHandler)
+
+    self.npx_x = npc.npx_x
+    self.npc_y = npc.npc_y
+    self.display_text = npc.display_text
+    self.isText = true
+
+    
+end
+
+function TextBox:update()
+    Object.update(self)
+end
+
+function TextBox:draw()
+
+    
+    love.graphics.setColor(1,1,1,1)
+    love.graphics.rectangle("line", self.x , self.y , self.w, self.h)
+
+    love.graphics.printf("HenloHenloHenloHenloHenloHenloHenloHenloHenloHenloHenloHenloHenlo", self.x, self.y, self.w, 'center')
+
+    
+
+end
